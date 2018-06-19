3fa1cabab4f439e8948ab7255a6641e837dac0c4
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-19 21:29:45 +0100

    Fix lint errors in app.js

--- server/app.js
+++ server/app.js
@@ -1,29 +1,29 @@
-import express from "express";
-import bodyParser from "body-parser";
-import http from "http";
-import logger from "morgan";
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import logger from 'morgan';
 
-//Setup express app
+// Setup express app
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-//Parse incoming requests
+// Parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = parseInt(process.env.PORT, 10) || 8000;
-app.set("port", port); 
+app.set('port', port);
 
-//Create the server
+// Create the server
 const server = http.createServer(app);
 
-server.listen(port, (req, res) => {
-  console.log(`Server is running on localhost:${port}`)
+server.listen(port, () => {
+  console.log(`Server is running on localhost:${port}`);
 });
 
-app.get("*", (req, res) => res.status(200).send({
-  Success: "Welcome to Ride My Way API"
+app.get('*', (req, res) => res.status(200).send({
+  Success: 'Welcome to Ride My Way API'
 }));
 
 export default app;
