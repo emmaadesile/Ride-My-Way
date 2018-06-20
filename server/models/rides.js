1da904b7162ff2b53f07812d0ed1a0e3bbfb49ac
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-20 08:33:07 +0100

    Create dummy data for the api endpoints

---
+++ server/models/rides.js
@@ -0,0 +1,28 @@
+const rides = [
+  {
+    id: 1,
+    destination: 'Yaba to Victoria Island',
+    time: '1:00pm',
+    passengers: 4,
+  },
+  {
+    id: 2,
+    destination: 'Ikorodu to CMS',
+    time: '10:00am',
+    passengers: 2,
+  },
+  {
+    id: 3,
+    destination: 'Obalende to Ajah',
+    time: '11:00am',
+    passengers: 7,
+  },
+  {
+    id: 4,
+    destination: 'Maryland to Ikeja',
+    time: '4:00pm',
+    passengers: 5,
+  }
+];
+
+export default rides;
