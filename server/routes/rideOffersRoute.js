0578a266259c835fc724fd3fb513f36c0542c663
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-20 12:08:59 +0100

    Get a single offer endpoint is working

--- server/routes/rideOffersRoute.js
+++ server/routes/rideOffersRoute.js
@@ -5,6 +5,9 @@ import RideOffersController from '../controllers/rideOffersController';
 const routes = (app) => {
   // Get all ride offers routes
   app.get('/api/v1/rideOffers', RideOffersController.getAllRideOffers);
+
+  // Get a single ride offer
+  app.get('/api/v1/rideOffers/:rideOfferId', RideOffersController.getASingleRideOffer);
 };
 
 export default routes;
