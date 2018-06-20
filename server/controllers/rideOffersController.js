9e281c5aeea9947ece0166c6a487f9d5ffed6a02
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-20 12:05:36 +0100

    Create getASingleRideOffer method for getting a single ride offer

--- server/controllers/rideOffersController.js
+++ server/controllers/rideOffersController.js
@@ -11,6 +11,22 @@ class RideOffersController {
   static getAllRideOffers(req, res) {
     return res.status(200).json({ rideOffers });
   }
+
+  /**
+ * Get A Single Ride Offer
+ * @param {obj} req
+ * @param {obj} res
+ * @returns All a single ride offer in db
+ * @memberof RideOffersController
+ */
+  static getASingleRideOffer(req, res) {
+    const rideOfferResult = rideOffers.find(rideOffer => (
+      rideOffer.id === parseInt(req.params.rideOfferId, 10)
+    ));
+    return rideOfferResult
+      ? res.status(200).json({ rideOfferResult })
+      : res.status(404).json({ error: 'Ride Offer not found' });
+  }
 }
 
 export default RideOffersController;
