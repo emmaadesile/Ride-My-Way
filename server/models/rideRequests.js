977dc224da7640d80b0f416b97e82ca625add919
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-21 11:17:59 +0100

    Create ridde request model

--- server/models/rideRequests.js
+++ server/models/rideRequests.js
@@ -9,6 +9,39 @@ const rideRequests = [
         passengersId: [1, 3]
       }
     ]
+  },
+  {
+    id: 2,
+    noOfSeats: 8,
+    seatsAvailable: 3,
+    requests: [
+      {
+        noOfRequests: 1,
+        passengersId: [1, 3]
+      }
+    ]
+  },
+  {
+    id: 3,
+    noOfSeats: 4,
+    seatsAvailable: 2,
+    requests: [
+      {
+        noOfRequests: 1,
+        passengersId: [1, 3]
+      }
+    ]
+  },
+  {
+    id: 4,
+    noOfSeats: 10,
+    seatsAvailable: 2,
+    requests: [
+      {
+        noOfRequests: 2,
+        passengersId: [1, 3]
+      }
+    ]
   }
 ];
 
