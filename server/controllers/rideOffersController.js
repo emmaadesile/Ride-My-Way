53792b48c8e035de231f7db2450174d8c8b65788
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-29 15:18:52 +0100

    ft(Database): get a single ride offer
    - fetch the details of a ride from the database
    
    [(Delivers) #158724091]

--- server/controllers/rideOffersController.js
+++ server/controllers/rideOffersController.js
@@ -24,6 +24,31 @@ class RideOffersController {
       });
     });
   }
+
+  /**
+ * Get A Ride Offer
+ * @param {obj} req
+ * @param {obj} res
+ * @returns A Single rides in db
+ * @memberof RideOffersController
+ */
+  static getASingleRideOffer(req, res) {
+    const rideId = parseInt(req.params.rideId);
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(`cannot connect to database: ${err}`);
+        res.status(400).send(err);
+      }
+      client.query(`SELECT * FROM rides WHERE id = ${rideId}`, (err, result) => {
+        done();
+        if (err) {
+          console.log(err);
+          res.status(400).send(err);
+        }
+        res.status(200).send(result.rows);
+      });
+    });
+  }
 }
 
 export default RideOffersController;
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,19 +15,19 @@ const routes = (app) => {
   app.get('/api/v1/rides/:rideId', RideOffersController.getASingleRideOffer);
 
   // Post a ride offer
-  app.post('/api/v1/rides', RideOffersController.createARideOffer);
+  // app.post('/api/v1/rides', RideOffersController.createARideOffer);
 
   // Edit a ride offer
-  app.put('/api/v1/rides/:rideId', RideOffersController.editRideOffer);
+  // app.put('/api/v1/rides/:rideId', RideOffersController.editRideOffer);
 
   // Delete a ride offer
-  app.delete('/api/v1/rides/:rideId', RideOffersController.deleteRideoffer);
+  // app.delete('/api/v1/rides/:rideId', RideOffersController.deleteRideoffer);
 
   // Request to join a ride offer
-  app.post('/api/v1/rides/:rideId/requests', RideRequestsController.requestToJoinARideOffer);
+  // app.post('/api/v1/rides/:rideId/requests', RideRequestsController.requestToJoinARideOffer);
 
   // View Ride Requests
-  app.get('/api/v1/rideRequests', RideRequestsController.getAllRideRequests);
+  // app.get('/api/v1/rideRequests', RideRequestsController.getAllRideRequests);
 };
 
 export default routes;
