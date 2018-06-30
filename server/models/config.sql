1dfb00f3e1c41a8b55444d4f2a139ae612e1dad1
Emmanuel Adesile <emma2adesile@gmail.com>
2018-06-30 06:32:29 +0100

    feat(Database): Setup the database
    - create a .sql schema in models
    - rename pool to dbConfig
    - profile link on ride page goes to users profile
    - fix errors with eslint
    
    [(Delivers) #158650357]

---
+++ server/models/config.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  phone INT NOT NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE rides (
+  id SERIAL PRIMARY KEY,
+  location VARCHAR(255) NOT NULL,
+  destination VARCHAR(255) NOT NULL,
+  departure_time TIME NOT NULL,
+  time_created DATE NOT NULL DEFAULT CURRENT_DATE,
+  seats_available INT NOT NULL
+);
+
+
+CREATE TABLE ride_requests(
+  id SERIAL PRIMARY KEY,
+  ride_id INT NOT NULL,
+  user_id NOT NULL
+);
