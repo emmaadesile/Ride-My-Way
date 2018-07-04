992750bab6a262d1cbb2d4bc873dcbfed201dd38
Emmanuel Adesile <emma2adesile@gmail.com>
2018-07-04 10:18:46 +0100

    feat(Sign in): user can sign in after being authenticated
    - encrypt user's password
    - authenticate user with jwt
    - return error if user is not found
    
    [(Delivers) #158701371]

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,16 +2,12 @@ import pool from '../models/dbConfig';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import config from '../helpers/config';
+import TokenAuth from '../helpers/token';
 
-<<<<<<< HEAD
-const secretKey = process.env.SECRET_KEY;
-=======
 dotenv.config();
 
-const secretKey = process.env.SECRET_KEY;
+const secret = process.env.SECRET_KEY;
 
->>>>>>> 3787a611ffe6fc520413c4d5e74ffec7f3ff1534
 
 class UsersController {
   /**
@@ -28,36 +24,39 @@ class UsersController {
     const hashedPassword = bcrypt.hashSync(password, 10);
     password = hashedPassword;
 
+    // declare the sign up query
     const query = {
       text: 'INSERT INTO users(firstname, lastname, username, email, password) VALUES($1, $2, $3, $4, $5) RETURNING *',
-      values: [`${firstname}`, `${lastname}`, `${username}`, `${email}`, `${password}`],
+      values: [firstname, lastname, username, email, hashedPassword],
     };
+    // connect to database
     pool.connect((err, client, done) => {
       if (err) {
         done();
-        res.status(500).json({ success: false, error: err });
+        res.status(500).json({ success: false, message: 'There seems to be an error on the server' }); // error in the database
       }
       client.query(query, (err, result) => {
         if (err) {
           res.status(400).json({ success: false, message: 'There was an error registering the user' });
         }
-        if (result) {
-          client.query('SELECT user_id from users WHERE email = $1', [email], (err, result) => {
-            if (err) {
-              res.status(400).send('cannot connect');
-            }
-            if (result) {
-              const token = jwt.sign({ id: result.rows[0].user_id }, secretKey, {
-                expiresIn: 86400
-              });
-              res.status(200).send({
-                auth: true,
-                token,
-                result: result.rows,
-              });
-            }
-          });
-        }
+        // select the new user from database     
+        client.query('SELECT user_id from users WHERE email = $1', [email], (err, result) => {
+          if (err) {
+            res.status(400).send('cannot connect');
+          }
+          if (result) {
+            const userId = result.rows[0].user_id;
+            const userToken = TokenAuth.makeToken(userId);
+            // const token = jwt.sign({ userId }, secret, {
+            //   expiresIn: "24h"
+            // });
+            res.status(200).send({
+              auth: true,
+              userToken,
+              result: result.rows,
+            });
+          }
+        });        
       });
     });
   }
@@ -77,25 +76,34 @@ class UsersController {
         done();
         res.status(500).send({ success: false, error: err });
       }
-      client.query(`SELECT password from users WHERE email = ${email}`, (err, result) => {
+      client.query("SELECT * from users WHERE email = $1", [email], (err, result) => {
         if (err) {
-          res.status(401).json({ success: false, message: 'Problem connecting to server' });
+          res.status(500).json({ success: false, message: 'There seems to be an error on the server' });
         }
-        if (result) {
-          const passWordIsValid = bcrypt.compareSync(password, result.password);
-          if (passWordIsValid) {
-            return res.status(401).send({
+        const user = result.rows[0];
+        if (!user) {
+          return res.status(404).json({
             auth: false,
-            token: null
-            });
-          } 
-          const token = jwt.sign({ id: result.row[0].user_id }, config.secretKey, { expiresIn: 86400 });
-          res.status(200).send({
-            auth: true, token
+            error: "User not found"
           });
         }
-      })
-    })
+        // if user exists in database
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) {
+          return res.status(401).json({
+            auth: false,
+            token: null,
+            error: "You entered the wrong passowrd"
+          });
+        }
+        const userId = result.rows[0].user_id;
+        const userToken = TokenAuth.makeToken(userId);
+        res.status(200).send({
+          auth: true,
+          userToken
+        });
+      });
+    });
   }
 }
 
--- server/models/dbConfig.js
+++ server/models/dbConfig.js
@@ -3,25 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-<<<<<<< HEAD
-// const dbConfig = new Pool({
-//   user: process.env.DB_USERNAME,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-//   min: 0,
-//   max: 10, // max number of connection that can be open to database
-//   idleTimeoutMills: 3000 // how long client is allowed to remain idle before being closed
-// });
-
-// dbConfig.connect();
-
-const dbConfig = {
-  user: 'postgres',
-=======
 const pool = new Pool({
   user: process.env.DB_USERNAME,
->>>>>>> 3787a611ffe6fc520413c4d5e74ffec7f3ff1534
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
