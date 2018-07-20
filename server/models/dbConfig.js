70a25ceabb2a719e2035375a8c59003edf358541
Emmanuel Adesile <emma2adesile@gmail.com>
2018-07-20 22:07:17 +0100

    Run app from dist folder

--- server/models/dbConfig.js
+++ server/models/dbConfig.js
@@ -1,22 +1,11 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-import config from '../config/config';
 
 dotenv.config();
 
-const environment = process.env.NODE_ENV;
-
-let pool;
-
-if (environment === 'development') {
-  pool = new Pool(config.development);
-}
-if (environment === 'test') {
-  pool = new Pool(config.test);
-}
-
-if (environment === 'production') {
-  pool = new Pool(config.production);
-}
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 export default pool;
