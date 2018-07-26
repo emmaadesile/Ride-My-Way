04502bf9cedd2c12006165d799e2ba87c48b6395
Emmanuel Adesile <emma2adesile@gmail.com>
2018-07-26 22:42:35 +0100

    ft(signup-endpoint): consume sign in endpoint
    - add validation to sign in page
    - modify the validation for sign up page
    - show status of sign in after request is sent
    - add lint-all command to fix all linting errors
    - delete validate.js file
    - redesign user profile page
    
    [(Delivers) #159216107]

--- client/validate.js
+++
@@ -1,8 +0,0 @@
-const signupForm = document.querySelector('.signup-form');
-const signupFormInput = signupForm.querySelectorAll('input');
-const signinForm = document.querySelector('.signin-form');
-const signinFormInput = signinForm.querySelectorAll('input');
-const signupButton = signupForm.querySelector('.signup-btn');
-const signinButton = signupForm.querySelector('.signin-btn');
-
-
