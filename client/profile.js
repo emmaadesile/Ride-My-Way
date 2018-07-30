4018f1aaa9f855c58a0f49ea241925e5938521b1
Emmanuel Adesile <emma2adesile@gmail.com>
2018-07-30 17:51:14 +0100

    ft(signin-endpoint): Consume signin endpoint
    Registered user can sign into the app
    User profile is loaded after sign in
    User can go to rides page from their profile
    User can view all rides
    User can view the details of a ride
    
    [(Delivers) #159216107]

--- client/profile.js
+++ client/profile.js
@@ -1,18 +1,46 @@
-const profileName = document.querySelector(".profile-name");
-const userToken = sessionStorage.getItem("token");
+const profileName = document.querySelector('.profile-name');
+const signoutButton = document.querySelector('.signout-btn');
+const userToken = sessionStorage.getItem('x-access-token');
+const ridesButton = document.querySelector('.btn-rides');
 
+// get user info from token
 function decodeToken(token) {
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace("-", "+").replace("_", "/");
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace('-', '+').replace('_', '/');
   return JSON.parse(window.atob(base64));
 }
 
-const userInfo = decodeToken(token);
+// get the users info from the token provided
+const userInfo = decodeToken(userToken);
 const userId = userInfo.userId.user_id;
 const firstname = userInfo.userId.firstname;
-const lastname = userId.user_id.lastname;
+const lastname = userInfo.userId.lastname;
 const fullname = `${firstname} ${lastname}`;
+console.log(fullname);
 
-(function (){
-  profileName.innerHTML = fullname;
-})();
+/**
+ * Signs in a registered user
+ * @param {obj} req
+ * @param {obj} res
+ * @returns a users profile
+ */
+function loadProfile() {
+  profileName.textContent = fullname;
+}
+
+loadProfile();
+
+// rides button redirects to rides page
+function ridesPageRedirect() {
+  window.location('./rides.html');
+}
+
+// sign out function
+function signout() {
+  sessionStorage.clear();
+  window.location('./signin.html');
+}
+
+// event listeners
+ridesButton.addEventListener('click', ridesPageRedirect);
+signoutButton.addEventListener('click', signout);
